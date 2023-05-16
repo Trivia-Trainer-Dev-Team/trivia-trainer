7bbc15f410557064bd1a09392724d43a816d778b
slimesown <plee50@binghamton.edu>
2023-05-16 19:41:30 -0400

    Quizzes/Home page completed
    Co-authored-by: Justin Poirier <jcpoirier20@users.noreply.github.com>

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 //------>Full Page<-----
@@ -31,12 +31,12 @@ function QuizTitle() {
 
 function QuizElements() {
   // reformat this to use elements that can navigate to specific urls.
-  const elements = ['Movies', 'Science', 'c', 'd', 'e'];
+  const elements = ['Sports', 'Mathematics', 'Computers'];
 
   return (
     <div id='Quizzes'>
       {elements.map((el, i) => {
-        <NavLink key={i} to={`/quizPage/${el}`}>
+        <NavLink key={i} to={`/QuizPage/${el}`}>
           <span>{el}</span>
         </NavLink>;
       })}
@@ -48,21 +48,37 @@ function QuizElements() {
 
 //------->Center Element<--------
 function UserContainer() {
+const [name, setName] = useState('');
+const [right, setRight] = useState('');
+
+// useEffect(()=>{
+//     const retrieveUserData = async() =>{
+//         try {
+//             const response = await fetch('/users');
+//             const data = await response.json();
+//             const {questionsRight, name} = data;
+//             setName(name);
+//             setRight(questionsRight);
+//         } catch (err){
+//             console.log(err);
+//         }
+//     }
+// })
+
+
   return (
     <div>
-      <CenterUserData />
-      <UserNav />
+      <CenterUserData right={right}/>
+      <UserNav name={name}/>
     </div>
   );
 }
 
-function CenterUserData() {
+function CenterUserData({right}) {
   return (
     <div>
       {/* These will be used as get requests later on. */}
-      <span>5</span>
-      <span>/</span>
-      <span>10</span>
+      <span>{right}</span>
       <h5>Questions Correct</h5>
     </div>
   );
@@ -70,11 +86,11 @@ function CenterUserData() {
 
 //------->Center/Right Element<--------
 
-function UserNav() {
+function UserNav({name}) {
   return (
     <div>
       <UserImage />
-      <UserBody />
+      <UserBody name={name}/>
     </div>
   );
 }
@@ -83,8 +99,8 @@ function UserImage() {
   return <img src='#'></img>;
 }
 
-function UserBody() {
-  return <p>Hello World!</p>;
+function UserBody({name}) {
+  return <p>{name}</p>;
 }
 
 export default HomeElement;
