8797548fd84c4fb0a8c082aaf0941654d4500c85
slimesown <plee50@binghamton.edu>
2023-05-17 11:57:04 -0400

    Question are loading, and iterating through

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -4,73 +4,63 @@ import { useParams } from 'react-router-dom';
 function QuizPage() {
   const { genre } = useParams();
 
-  // create state object for entire list of quiz questions
-  // create state for current quiz question object
   const [quizList, setQuizList] = useState([]);
   const [currQuestion, setCurrQuestion] = useState({});
+  const [loading, setLoading] = useState(false);
   const [quizScore, setQuizScore] = useState(0);
+  const [index, setIndex] = useState(0);
 
-  // create generic index variable to track moving through the quiz array
-  let index = 0;
+  const fetchQuizData = () => {
+    setLoading(true);
+    fetch(`/questions/${genre}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuizList(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
 
-  // universal function to handle button clicks from any quiz answer choice
+  useEffect(() => {
+    fetchQuizData();
+  }, []);
+
+  useEffect(() => {
+    if (quizList.length > 0) {
+      setCurrQuestion(quizList[index]);
+    }
+  }, [quizList, index]);
+
   const answerSubmission = (event) => {
-    // pull the data from the selected button
     const answer = event.target.value;
-    // if the data matches the real answer, update quizScore and userScore state
+    setIndex((prevIndex) => prevIndex + 1);
+
     if (answer === currQuestion.correct_answer) {
-      setQuizScore((prevState) => (prevState += 1));
-      updateUserScore();
+      setQuizScore((prevScore) => prevScore + 1);
     }
   };
-  // set currentQuestion to the quizList[++index]
-  // render a quiz question from currentQuestion information
-  useEffect(async () => {
-    try {
-      const response = await fetch(`/questions/${genre}`);
-      const data = await response.json();
-      if (response.ok) {
-        setQuizList(data);
-      }
-      console.log(quizList);
-    } catch (err) {
-      return `Error fetching quiz list in QuizPage.jsx. Error: ${err}`;
-    }
-  });
-
-  // useEffect(() => {
-  //   setCurrQuestion(quizList[index]);
-  // }, [quizList]);
 
   return (
-    <div id='question-card'>
-      <div id='question-header'>{currQuestion?.question}</div>
-      <div id='question-body'>
-        <button id='answer1' onClick={answerSubmission}>
-          {currQuestion?.correct_answer}
-        </button>
-        <button id='answer2' onClick={answerSubmission}>
-          {currQuestion?.incorrect_answers}
-        </button>
-      </div>
-      <div id='question-footer'>
-        <button id='quit-question'>Quit</button>
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <div>...Data Loading...</div>
+      ) : (
+        <div id='question-card'>
+          <div>{currQuestion.question}</div>
+          <button
+            onClick={answerSubmission}
+            value={currQuestion.correct_answer}
+          >
+            Submit Answer
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 
 export default QuizPage;
-
-/* {
-  category: 'science: computers',
-  type: 'multiple',
-  diffulty: 'medium',
-  question: 'On which day did the World Wide Web go online?',
-  correct_answer: 'December 20, 1990',
-  incorrect_answers: [
-  'December 17, 1996',
-  'November 12, 1990',
-  'November 24, 1995',
-  ]
-  }, */
