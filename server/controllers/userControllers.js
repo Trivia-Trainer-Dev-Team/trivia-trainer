df138a465dfb2d3a2a925f7b553882220c8ad4fb
Fabrizzio Quintanilla <fabrizzioquintanilla@Fabrizzios-MBP.home>
2023-05-16 20:08:04 -0400

    added the controller and model

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,96 @@
+const mongoose = require('mongoose');
+const User = require('../models/userModel.js');
+
+
+const userController = {};
+
+
+//USER SIGN UP -
+userController.createUser = async (req, res, next) => {
+   const { username, password, name } = req.params;
+   if (!username || !password || !name) {
+       return next({
+           log: 'Error occurred in userController.createUser',
+           status: 400,
+           message: { err: 'An error occurred' }
+       });
+   }
+   try {
+       console.log(req.body)
+       res.locals.user = await User.create({ //Users will input their username, password, and name, score d -> 0
+           username, password, name
+       });
+       return next();
+   } catch (err) {
+       return next({
+           log: 'Error occurred in userController.createUser',
+           status: 400,
+           message: { err: 'An error occurred' },
+       });
+   }
+};
+
+
+
+
+//USER LOGIN
+userController.verifyUser = async (req, res, next) => {
+   const { username, password } = req.params; //all we need is these two to login
+   console.log(req.body)
+   if (!username || !password) {
+       return next({
+           log: 'Error occurred in userController.verifyUser',
+           status: 400,
+           message: { err: 'An error occurred in entering username/password in verifyUser' }
+       });
+   }
+   try {
+       const user = await User.findOne({ username });
+       if (!user) {
+         return next({
+           log: 'Error occurred in userController.verifyUser',
+           status: 400,
+           message: { err: 'An error occurred, cannot find user' }
+         });
+       }
+       res.locals.user = user;
+       return next();
+     } catch (err) {
+       return next({
+         log: 'Error occurred in userController.verifyUser',
+         status: 400,
+         message: { err: 'An error occurred' },
+       });
+     }
+}
+
+
+userController.updateScore = async (req, res, next) => {
+   const { numOfCorrectQuestions, username, name } = req.params; //When you finish the quiz, it should send over the username of the person and the #of correct questions
+   try {
+     const user = await User.findOne({ username });
+     if (user) {
+       user.increaseScore(numOfCorrectQuestions);
+       res.locals.updatedScore = { score: user.score };
+       await user.save(); // Save the updated user to the database   
+       return next()
+     } else {
+       return next({
+           log: 'Error occurred in userController.updateScore',
+           status: 400,
+           message: {error: "User not found"}
+       })
+     }
+   } catch (error) {
+       return next({
+           log: 'Error occurred in userController.updateScore',
+           status: 400,
+           message: { err: 'An error occurred' },
+       });
+   }
+ };
+
+
+
+
+module.exports = userController
