5482d7ea7e5ddd0777a582274788680812216df9
Fabrizzio Quintanilla <fabrizzioquintanilla@Fabrizzios-MBP.home>
2023-05-17 11:49:11 -0400

    why is this still on staged

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -6,15 +6,15 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema ({
-   username: {type: String, required:true},
-   password: {type: Number, required: true},
+   username: {type: String, required:true, unique:true},
+   password: {type: String, required: true},
    name: {type: String, required: true},
    score: {type: Number, default: 0} //every person starts with a score of 0
 })
 
 
 userSchema.methods.increaseScore = function(numCorrectQuestions) {
-   this.score += numCorrectQuestions;
+   this.score += 1;
    return this.save();
  };
 
