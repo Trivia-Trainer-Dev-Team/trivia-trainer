6df8fe3b8b2c6e1ba672864f78eb29f9fe6b7c2b
slimesown <plee50@binghamton.edu>
2023-05-18 11:55:23 -0400

    added Music, Games, Television categories

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,28 +1,26 @@
-
-
 const apiController = {};
 apiController.retrieveData = async (req, res, next) => {
   try {
     console.log('Trying to retrieve data');
     const { category } = req.params;
-    let number;
-    if (category === 'Sports') {
-      number = 21;
-    } else if (category === 'Computers') {
-      number = 18;
-    } else if (category === 'Mathematics') {
-      number = 19;
-    }
-    const url = `https://opentdb.com/api.php?amount=10&category=${number}&difficulty=medium&type=multiple`;
-    fetch(url)
-    .then(response => response.json())
-    .then(data =>{
-      console.log(data);
-      console.log('this is the result array' + data.results);
-      res.locals.result = data.results;
-      return next();
-    })
+    const numberObj = {
+      Sports: 21,
+      Computers: 18,
+      Mathematics: 19,
+      Music: 12,
+      Games: 15,
+      Television: 14,
+    };
 
+    const url = `https://opentdb.com/api.php?amount=10&category=${numberObj[category]}&difficulty=medium&type=multiple`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        console.log('this is the result array' + data.results);
+        res.locals.result = data.results;
+        return next();
+      });
   } catch (err) {
     return next({
       log: 'An error occured in in apiController on retrieveData middleware function',
